9d3a746e2d146decb3693f7f1dd6676ea668ef09
felipefonseca2079 <fonsecafelipe295@gmail.com>
2026-06-11 21:17:06 -0300

    Update Jogador.java

--- src/main/java/Jogador.java
+++ src/main/java/Jogador.java
@@ -15,3 +15,23 @@ public class Jogador {
         return this.nome;
     }
 }
+public abstract class Jogador implements JogadorInterface {
+    private String nome;
+    private char cor;
+
+    public Jogador(String nome, char cor) {
+        if (cor != 'p' && cor != 'b') {
+            throw new IllegalArgumentException("A cor deve ser 'p' (preta) ou 'b' (branca).");
+        }
+        this.nome = nome;
+        this.cor = cor;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public char getCor() {
+        return this.cor;
+    }
+}
