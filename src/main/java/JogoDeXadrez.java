08ad64f9b5e0c7c74f82480d40ccda34fdbea8e1
felipefonseca2079 <fonsecafelipe295@gmail.com>
2026-06-18 20:50:00 -0300

    Update JogoDeXadrez.java

--- src/main/java/JogoDeXadrez.java
+++ src/main/java/JogoDeXadrez.java
@@ -1,4 +1,5 @@
-    public static void main(String[] args) {
+    }class JogoDeXadrez implements Jogo {
+public static void main(String[] args) {
         JogoDeXadrez partida = new JogoDeXadrez();
         partida.iniciar();
         System.out.println("SEU NOME COMPLETO");
